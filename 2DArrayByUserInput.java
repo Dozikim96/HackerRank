17769b46201b3e842889bac14537122b70b2d1d3
Bharat Sharma <bs524942@gmail.com>
2026-05-19 09:02:15 +0530

    Implement user input for 2D array creation
    This program takes user input to create a 2D array and displays its elements.

---
+++ 2DArrayByUserInput.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of Rows: ");
+        int row = sc.nextInt();
+        System.out.print("Enter the no of column: ");
+        int col = sc.nextInt();
+        
+        int[][] arr = new int[row][col];
+        System.out.println("Enter the element of array: ");
+        
+        for(int i=0; i<row;i++){
+            for(int j=0;j<col;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The Element of array is: ");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
