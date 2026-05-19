38c026fdfd47f87d6a89c9f9f9a9c708f42ae64d
Bharat Sharma <bs524942@gmail.com>
2026-05-19 17:49:29 +0530

    Implement majority element algorithm

---
+++ 169. Majority Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int n =0;
+
+        for(int i =0; i<nums.length;i++){
+            if (count == 0){
+                n = nums[i];
+            }
+            if(nums[i] == n){
+                count++;
+            }else count--;
+        }
+        return n;
+    }
+}
